66bdb64f10304a4d4b884f8eb1af0ff43ea6eac9
y-takashina <takashina2051@gmail.com>
2016-09-08 01:03:44 +0900

    RichControls の中身を少し変えた

--- RichControls.RichTextBox.cs
+++ RichControls.RichTextBox.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace RichControls
 {
-    public static class RichTextBox
+    public static partial class Extensions
     {
 
         public static void Write(this TextBox self, string str="")
