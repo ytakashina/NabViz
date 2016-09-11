e03ea41a4be937f237332b6c20f207e0df3a9b36
y-takashina <takashina2051@gmail.com>
2016-09-11 20:16:03 +0900

    閾値法を実装したはず

--- Detectors.cs
+++ Detectors.cs
@@ -5,7 +5,44 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace ZetaOne
 {
-    class Algorithms
+    public static partial class Extensions
+    {
+        public static double StandardDeviation(this IEnumerable<double> values)
+        {
+            var enumerable = values as double[] ?? values.ToArray();
+            if (!enumerable.Any()) return 0;
+            double avg = enumerable.Average();
+            double sum = enumerable.Sum(x => Math.Pow(x - avg, 2));
+            return Math.Sqrt(sum / (enumerable.Length - 1));
+        }
+
+        public static double Erf(double x)
+        {
+            const double a1 = 0.254829592;
+            const double a2 = -0.284496736;
+            const double a3 = 1.421413741;
+            const double a4 = -1.453152027;
+            const double a5 = 1.061405429;
+            const double p = 0.3275911;
+
+            // Save the sign of x
+            var sign = Math.Sign(x);
+            x = Math.Abs(x);
+
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return sign * y;
+        }
+
+        public static double Erfc(double x)
+        {
+            return 1 - Erf(x);
+        }
+    }
+
+    class Detectors
     {
         // SlidingThrethold (WindowedGaussian)
         // 閾値法
@@ -40,67 +77,42 @@ namespace ZetaOne
                 if (_windowData.Count < _windowSize)
                 {
                     _windowData.Add(inputValue);
-                    _updateWindow();
+                    UpdateWindow();
                 }
                 else
                 {
                     _stepBuffer.Add(inputValue);
                     if (_stepBuffer.Count == _stepSize)
                     {
+                        _windowData.RemoveRange(0, _stepSize);
+                        _windowData.AddRange(_stepBuffer);
+                        _stepBuffer.Clear();
+                        UpdateWindow();
                     }
                 }
-
                 return anomaryScore;
             }
 
-            private void _updateWindow()
+            private void UpdateWindow()
             {
                 _mean = _windowData.Average();
-
+                _standardDeviation = _windowData.StandardDeviation();
                 if (Math.Abs(_standardDeviation) < 0.000001)
                 {
                     _standardDeviation = 0.000001;
                 }
             }
 
-
-            static double Erf(double x)
-            {
-                const double a1 = 0.254829592;
-                const double a2 = -0.284496736;
-                const double a3 = 1.421413741;
-                const double a4 = -1.453152027;
-                const double a5 = 1.061405429;
-                const double p = 0.3275911;
-
-                // Save the sign of x
-                var sign = Math.Sign(x);
-                x = Math.Abs(x);
-
-                // A&S formula 7.1.26
-                double t = 1.0 / (1.0 + p * x);
-                double y = 1.0 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
-
-                return sign * y;
-            }
-
-            static double Erfc(double x)
-            {
-                return 1 - Erf(x);
-            }
-
-
-            static double NormalProbability(double x, double mean, double standardDeviation)
+            private static double NormalProbability(double x, double mean, double standardDeviation)
             {
                 if (x < mean)
                 {
                     var xp = 2 * mean - x;
                     return 1.0 - NormalProbability(xp, mean, standardDeviation);
                 }
-                var z = (x - mean)/standardDeviation;
-                return 0.5*Erfc(z / Math.Sqrt(2));
+                var z = (x - mean) / standardDeviation;
+                return 0.5 * Extensions.Erfc(z / Math.Sqrt(2));
             }
         }
-
     }
 }
