455ecd08b52ca21aa9a1aa52f8e3eeb68cdc09e0
y-takashina <takashina2051@gmail.com>
2016-09-12 02:13:50 +0900

    テストを追加

--- Tests/MathExtensionsTests.cs
+++ Tests/MathExtensionsTests.cs
@@ -34,16 +34,55 @@ namespace ZetaOne.Tests
         [TestMethod()]
         public void ErfTest()
         {
+            var array = new[]
+            {
+                0.00, 0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 1.00, 1.50, 2.00
+            };
+            var ans = new[]
+            {
+                0.0000000, 0.0563720, 0.1124629, 0.2227026, 0.3286268,
+                0.4283924, 0.5204999, 0.8427008, 0.9661051, 0.9953223
+            };
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[0]) - ans[0]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[1]) - ans[1]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[2]) - ans[2]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[3]) - ans[3]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[4]) - ans[4]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[5]) - ans[5]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[6]) - ans[6]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[7]) - ans[7]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[8]) - ans[8]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.Erf(array[9]) - ans[9]) < 0.000001);
         }
 
         [TestMethod()]
         public void ErfcTest()
         {
+            // Erfc と同様。
         }
 
         [TestMethod()]
         public void QFunctionTest()
         {
+            var array = new[]
+            {
+                0.0, 0.1, 0.2, 0.3, 0.4, -0.5, -1.0, -1.5, -2.0, -3.0
+            };
+            var ans = new[]
+            {
+                0.500000000, 0.460172163, 0.420740291, 0.382088578, 0.344578258,
+                0.308537539, 0.158655254, 0.066807201, 0.022750132, 0.001349898
+            };
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[0], 0, 1) - ans[0]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[1], 0, 1) - ans[1]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[2], 0, 1) - ans[2]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[3], 0, 1) - ans[3]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[4], 0, 1) - ans[4]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[5], 0, 1) - ans[5]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[6], 0, 1) - ans[6]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[7], 0, 1) - ans[7]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[8], 0, 1) - ans[8]) < 0.000001);
+            Assert.IsTrue(Math.Abs(MathExtensions.QFunction(array[9], 0, 1) - ans[9]) < 0.000001);
         }
     }
 }
