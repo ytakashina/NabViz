72d7c0355946cdac6df0b837a2f912b0f95de1fb
y-takashina <takashina2051@gmail.com>
2016-09-11 17:17:49 +0900

    不要なusingの削除

--- AnomalyLabels.cs
+++ AnomalyLabels.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ZetaOne
 {
