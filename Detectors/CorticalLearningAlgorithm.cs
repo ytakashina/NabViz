c890c394eb382e2047c458786e17c9f7d4a0c24b
y-takashina <takashina2051@gmail.com>
2016-09-26 23:19:36 +0900

    CLA のひな型を追加

---
+++ Detectors/CorticalLearningAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ZetaOne.Detectors
+{
+    class CorticalLearningAlgorithm : Detector
+    {
+        public override void Initialize()
+        {
+            
+        }
+
+        public override double AnomalyScore(DataPoint dataPoint)
+        {
+            return 0;
+        }
+
+        public override void Record(DataPoint dataPoint)
+        {
+            
+        }
+    }
+}
