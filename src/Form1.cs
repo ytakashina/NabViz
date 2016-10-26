eb92fcc46b151ebe427d092e9525d31f150062f0
ytakashina <takashina2051@gmail.com>
2016-10-26 14:43:11 +0900

    ちょっと整理

--- src/Form1.cs
+++ src/Form1.cs
@@ -142,15 +142,15 @@ namespace NabViz
             {
                 chart1.Width *= 2;
                 chart1.Height *= 2;
-                chart1.Dock = DockStyle.None;
                 foreach (var s in chart1.Series) s.MarkerSize *= 2;
                 foreach (var s in chart1.Series) s.BorderWidth *= 2;
+                chart1.Dock = DockStyle.None;
                 chart1.SaveImage(DateTime.Now.ToString("yyMMdd_hhmmss") + ".png", ChartImageFormat.Png);
                 chart1.Width /= 2;
                 chart1.Height /= 2;
-                chart1.Dock = DockStyle.Fill;
                 foreach (var s in chart1.Series) s.MarkerSize /= 2;
                 foreach (var s in chart1.Series) s.BorderWidth /= 2;
+                chart1.Dock = DockStyle.Fill;
             }
         }
     }
